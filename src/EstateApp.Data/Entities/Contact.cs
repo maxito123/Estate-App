2321986e1e7e2e5171fd0ffcbb2f877faa0ca4f5
maxito123 <belovedmaximus@gmail.com>
2020-06-13 00:05:15 +0100

    added application db context

---
+++ src/EstateApp.Data/Entities/Contact.cs
@@ -0,0 +1,9 @@
+namespace EstateApp.Data.Entities
+{
+    public class Contact : BaseEntity
+    {
+        public string State { get; set;}
+        public string LocalGovermentArea { get; set;}
+        
+    }
+}
---
+++ src/EstateApp.Data/Entities/Property.cs
@@ -0,0 +1,9 @@
+namespace EstateApp.Data.Entities
+{
+    public class Property : BaseEntity
+    {
+        public string Title { get; set; } // 3 bedroom flat at iyana ipaja
+           
+
+    }
+}
