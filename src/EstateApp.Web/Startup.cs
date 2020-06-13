83672bc92c77fbc94dc9ae0dc8e5684bad0ba20e
maxito123 <belovedmaximus@gmail.com>
2020-06-13 17:03:03 +0100

    created default users and roles

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using EstateApp.Data.DatabaseContexts.ApplicationDbContext;
 using EstateApp.Data.DatabaseContexts.AuthenticationDbContext;
+using EstateApp.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,12 +39,27 @@ namespace EstateApp.Web
                     sqlServerOptions => {sqlServerOptions.MigrationsAssembly("EstateApp.Data");
                 }
             ));
+            services.AddIdentity<ApplicationUser,IdentityRole>()
+            .AddEntityFrameworkStores<AuthenticationDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+              options.Password.RequireDigit = false;
+              options.Password.RequiredLength = 6;
+              options.Password.RequireLowercase = false;
+              options.Password.RequireNonAlphanumeric = false;
+              options.Password.RequireUppercase = false;
+              options.SignIn.RequireConfirmedEmail = false;
+              options.SignIn.RequireConfirmedPhoneNumber = false;  
+            });
+
 
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -65,6 +84,49 @@ namespace EstateApp.Web
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            MigrateDatabaseContexts(svp);
+            CreateDefaultRolesAndUsers(svp).GetAwaiter().GetResult();
+        }
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+            var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext>();
+            authenticationDbContext.Database.Migrate();
+
+            var applicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+            applicationDbContext.Database.Migrate();
+        }
+        public async Task CreateDefaultRolesAndUsers(IServiceProvider svp)
+        {
+            string[] roles = new string[] {"SystemAdministrator", "Agent", "User"};
+            var userEmail = "admin@estateapp.com";
+            var userPassword = "SuperSecretPassword@2020";
+
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach(var role in roles)
+            {
+                var roleExists = await roleManager.RoleExistsAsync(role);
+                if(!roleExists)
+                {
+                    await roleManager.CreateAsync(new IdentityRole{Name = role});
+                }
+            }
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await userManager.FindByEmailAsync(userEmail);
+            if(user is null)
+            {
+                user = new ApplicationUser
+                {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "+2348063644518",
+                    PhoneNumberConfirmed = true,
+                };
+
+                await userManager.CreateAsync(user, userPassword);
+                await userManager.AddToRolesAsync(user, roles);
+            }
         }
     }
 }
