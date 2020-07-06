8ca9367262be50fca6f50dc20929dcc1275e64de
maxito123 <belovedmaximus@gmail.com>
2020-07-06 01:43:15 +0100

    added login/logout and register functionality

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using EstateApp.Data.DatabaseContexts.ApplicationDbContext;
 using EstateApp.Data.DatabaseContexts.AuthenticationDbContext;
 using EstateApp.Data.Entities;
+using EstateApp.Web.Interfaces;
+using EstateApp.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -56,6 +58,7 @@ namespace EstateApp.Web
 
 
             services.AddControllersWithViews();
+            services.AddTransient<IAccountsService, AccountsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +79,7 @@ namespace EstateApp.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
