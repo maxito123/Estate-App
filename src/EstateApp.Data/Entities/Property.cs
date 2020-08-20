07c04fddfa945478441dcbbfe224a0cd845083ed
maxito123 <belovedmaximus@gmail.com>
2020-08-20 22:51:43 +0100

    added column to property entity

--- src/EstateApp.Data/Entities/Property.cs
+++ src/EstateApp.Data/Entities/Property.cs
@@ -3,7 +3,13 @@ namespace EstateApp.Data.Entities
     public class Property : BaseEntity
     {
         public string Title { get; set; } // 3 bedroom flat at iyana ipaja
-           
-
+        public string ImageUrl { get; set; }
+        public double Price { get; set;} 
+        public string Description { get; set;}
+        public int NumberOfRooms { get; set;}
+        public int NumberOfBaths { get; set;}
+        public int NumberOfToilets { get; set; }
+        public string Address { get; set; }
+        public string ContactPhoneNumber {get; set; }
     }
 }
