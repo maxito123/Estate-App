80cc3cebc1c9b0d7ebc18e46bf7429c3ca23a0e2
maxito123 <belovedmaximus@gmail.com>
2020-08-21 21:52:50 +0100

    added html to add properties page

--- src/EstateApp.Web/Controllers/PropertiesController.cs
+++ src/EstateApp.Web/Controllers/PropertiesController.cs
@@ -1,12 +1,25 @@
+using System;
+using EstateApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstateApp.Web.Controllers
 {
     public class PropertiesController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(PropertyModel model) 
+        {
+            throw new NotImplementedException();
+        }
     }
-}
+} 
---
+++ src/EstateApp.Web/Models/PropertyModel.cs
@@ -0,0 +1,17 @@
+namespace EstateApp.Web.Models
+{
+    public class PropertyModel
+    {
+       
+        public string Title { get; set; } // 3 bedroom flat at iyana ipaja
+        public string ImageUrl { get; set; }
+        public double Price { get; set;} 
+        public string Description { get; set;}
+        public int NumberOfRooms { get; set;}
+        public int NumberOfBaths { get; set;}
+        public int NumberOfToilets { get; set; }
+        public string Address { get; set; }
+        public string ContactPhoneNumber {get; set; }
+    
+    }
+}
